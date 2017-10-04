0d740de462f7fc07b38b5e7b7a0988bf8895c1d2
Chris Knight <chrisk314@gmail.com>
2017-10-04 17:53:16 +0100

    Adds setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='python_example_project',
+    version='0.2',
+    description='Demonstrates basic python project structure, documentation and continuous integration',
+    packages=find_packages()
+)
