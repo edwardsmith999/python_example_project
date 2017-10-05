024f34ca6371c1942b2001b9dabb9d137595eede
Chris Knight <chrisk314@gmail.com>
2017-10-05 19:09:06 +0100

    Removes magic strings

--- python_example_project/stream_proc/conftest.py
+++ python_example_project/stream_proc/conftest.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from python_example_project.stream_proc.file_stream import FileStreamStats
 
 TEST_FSSTAT_DATA = [1, 2, 3, 4, 5]
+TEST_FSSTAT_DATE, TEST_FSSTAT_DATE_FORMAT = '01-01-2000', '%d-%m-%Y'
 
 
 @pytest.fixture(scope="session")
@@ -16,5 +17,5 @@ def file_stats_object():
     class FileStreamStatsMock(FileStreamStats):
         def __init__(self):
             self._data = file_stats_test_data()
-            self.timestamp = datetime.strptime('01-01-2000', '%d-%m-%Y')
+            self.timestamp = datetime.strptime(TEST_FSSTAT_DATE, TEST_FSSTAT_DATE_FORMAT)
     return FileStreamStatsMock()
